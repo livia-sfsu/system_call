c3166321497b33a25990cd332d6da085f4a77859
Dang Nguyen <margonnguyen@gmail.com>
2019-10-12 01:44:33 -0700

    files

---
+++ qotd/qotd.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+#include <stdlib.h>
+
+asmlinkage long sys_qotd(void)
+{
+	const char *quoteSet[] ={
+	"sucess is going from failure to failure",
+	"live the questions now",
+	"be somebody who makes everybody feel like somebody",
+	"enjoy the litte thing",
+	"Stop looking for happiness in the same place you lost it",
+	"everyday is a fresh start",
+	"train your mind to see the good in every situation",
+	}
+	const size_t quoteCount = sizeof( quoteSet ) / sizeof(quoteSet[0]);
+	printk("%s\n", quoteSet[rand() % quoteCount] );
+	return 0;
+}
