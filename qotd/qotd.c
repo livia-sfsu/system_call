124d4db8e5f9044280a2e5fab2453314cc2865bf
Kevin Luong <notepadkevin@icloud.com>
2019-10-12 22:44:57 -0700

    Update qotd.c

--- qotd/qotd.c
+++ qotd/qotd.c
@@ -1,7 +1,14 @@
 #include <linux/kernel.h>
+#include <linux/random.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-asmlinkage long sys_hello(void)
-{
-        printk("Hello world\n");
-        return 0;
+const char* quote[] = {"Stay Hungry, Stay Foolish.",
+                       "Do you want to sell sugar water for the rest of your life, or do you want to come with me and change the world?",
+                       "My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time."}; 
+
+asmlinkage long sys_qotd(void){
+        srand((unsigned) time(&t));
+        printk(quote[rand()%3]);
+	return 0;
 }
